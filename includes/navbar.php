1d8dbff183d1d68af9620255dd7107c30a30b647
Gayan Nandasena <gsnandasena@gmail.com>
2024-08-06 18:50:31 +0530

    initial Setup

---
+++ includes/navbar.php
@@ -0,0 +1,30 @@
+
+    <div class="navbar flex flex-wrap items-center justify-between mx-auto px-20 pt-12 h-16">
+
+        <div class="logo"><a href="#" class=" tracking-widest text-2xl font-bold duration-500 hover: text-white ">M - L - P</a></div>
+
+        <ul class="links flex gap-8  w-auto">
+            <!-- <li><a href="" class=" font-medium not-italic text-lg duration-500 text-white no-underline hover:text-sky-500 hover:scale-105 hover:tracking-widest " >Home</a></li> -->
+            <li><a href="" class=" font-medium not-italic text-lg duration-500 text-white no-underline hover:text-sky-500 hover:scale-105 hover:tracking-widest " >ABOUT</a></li>
+            <li><a href="" class=" font-medium not-italic text-lg duration-500 text-white no-underline hover:text-sky-500 hover:scale-105 hover:tracking-widest " >GALLERY</a></li>
+            <!-- <li><a href="" class=" font-medium not-italic text-lg duration-500 text-white no-underline hover:text-sky-500 hover:scale-105 hover:tracking-widest " >Services</a></li> -->
+            <li><a href="" class=" font-medium not-italic text-lg duration-500 text-white no-underline hover:text-sky-500 hover:scale-105 hover:tracking-widest " >CONTACT</a></li>
+            <li><a href="" class=" font-medium not-italic text-lg duration-500 text-white no-underline hover:text-sky-500 hover:scale-105 hover:tracking-widest " >PACKAGES</a></li>
+        </ul>
+
+        <div class="toggle_btn text-white text-2xl cursor-pointer hidden">
+            <i class="fa-solid fa-bars"></i>
+        </div>
+
+    </div>
+
+    <div class="dropdown_menu absolute right-8 top-20 h-0 w-80 bg-transparent backdrop-blur-lg rounded-3xl overflow-hidden transition-height text-white">
+        <!-- <li class=" flex justify-center items-center p-3"><a href="" class=" duration-500 hover:tracking-widest hover:text-sky-500 hover:scale-105">Home</a></li> -->
+        <li class=" flex justify-center items-center p-3"><a href="" class=" duration-500 hover:tracking-widest hover:text-sky-500 hover:scale-105">ABOUT</a></li>
+        <li class=" flex justify-center items-center p-3"><a href="" class=" duration-500 hover:tracking-widest hover:text-sky-500 hover:scale-105">GALLERY</a></li>
+        <!-- <li class=" flex justify-center items-center p-3"><a href="" class=" duration-500 hover:tracking-widest hover:text-sky-500 hover:scale-105">SERVICES</a></li> -->
+        <li class=" flex justify-center items-center p-3"><a href="" class=" duration-500 hover:tracking-widest hover:text-sky-500 hover:scale-105">CONTACT</a></li>
+        <li class=" flex justify-center items-center p-3"><a href="" class=" duration-500 hover:tracking-widest hover:text-sky-500 hover:scale-105">PACKAGES</a></li>
+    </div>
+
+    
---
+++ index.php
@@ -0,0 +1,16 @@
+<?php include('includes/header.php'); ?>
+<?php include('includes/navbar.php'); ?>
+
+
+<div class="hero-section text-left h-auto flex flex-col justify-center items-start text-white mt-80" style="margin-left: 15%; margin-right: 15%;">
+    <h1 class=" text-5xl font-bold tracking-wide">MALCOLM LISMORE</h1>
+    <h2 class=" text-2xl tracking-widest mt-1">P H O T O G R A P H Y</h2>
+    <p class=" text-xs tracking-widest mt-4">From the wild Scottish coast to your special day.Capturing memories that last a lifetime.</p>
+</div>
+
+<!-- <section class="about-me text-white">
+    <h1>ABOUT</h1>
+    <P>Hi, <br> I'm Malcolm Lismore</P>
+</section> -->
+
+<?php include('includes/footer.php'); ?>
