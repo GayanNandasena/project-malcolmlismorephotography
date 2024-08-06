3fb96b480b2a8afc13c0582e3019a4c927fe8c0e
Gayan Nandasena <gsnandasena@gmail.com>
2024-08-06 20:46:48 +0530

    navbar changes

--- includes/navbar.php
+++ includes/navbar.php
@@ -1,5 +1,5 @@
 
-    <div class="navbar flex flex-wrap items-center justify-between mx-auto px-20 pt-12 h-16">
+    <div class="navbar flex flex-wrap items-center justify-between mx-auto pl-20 pr-12 pt-12 h-16">
 
         <div class="logo"><a href="#" class=" tracking-widest text-2xl font-bold duration-500 hover: text-white ">M - L - P</a></div>
 
@@ -9,7 +9,7 @@
             <li><a href="" class=" font-medium not-italic text-lg duration-500 text-white no-underline hover:text-sky-500 hover:scale-105 hover:tracking-widest " >GALLERY</a></li>
             <!-- <li><a href="" class=" font-medium not-italic text-lg duration-500 text-white no-underline hover:text-sky-500 hover:scale-105 hover:tracking-widest " >Services</a></li> -->
             <li><a href="" class=" font-medium not-italic text-lg duration-500 text-white no-underline hover:text-sky-500 hover:scale-105 hover:tracking-widest " >CONTACT</a></li>
-            <li><a href="" class=" font-medium not-italic text-lg duration-500 text-white no-underline hover:text-sky-500 hover:scale-105 hover:tracking-widest " >PACKAGES</a></li>
+            <li><a href="" class=" font-medium not-italic text-lg duration-500 text-sky-500 no-underline border border-sky-500 px-5 py-2.5 hover:text-white hover:scale-105 hover:bg-sky-500" >PACKAGES</a></li>
         </ul>
 
         <div class="toggle_btn text-white text-2xl cursor-pointer hidden">
@@ -24,7 +24,7 @@
         <li class=" flex justify-center items-center p-3"><a href="" class=" duration-500 hover:tracking-widest hover:text-sky-500 hover:scale-105">GALLERY</a></li>
         <!-- <li class=" flex justify-center items-center p-3"><a href="" class=" duration-500 hover:tracking-widest hover:text-sky-500 hover:scale-105">SERVICES</a></li> -->
         <li class=" flex justify-center items-center p-3"><a href="" class=" duration-500 hover:tracking-widest hover:text-sky-500 hover:scale-105">CONTACT</a></li>
-        <li class=" flex justify-center items-center p-3"><a href="" class=" duration-500 hover:tracking-widest hover:text-sky-500 hover:scale-105">PACKAGES</a></li>
+        <li class=" flex justify-center items-center p-3"><a href="" class=" duration-500 hover:tracking-widest hover:scale-105  text-sky-500 no-underline border border-sky-500 px-5 py-2.5 hover:text-white">PACKAGES</a></li>
     </div>
 
     
