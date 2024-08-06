ae7f9ee7ed81981c77d33f1f7364d67e0ad82016
Gayan Nandasena <gsnandasena@gmail.com>
2024-08-07 03:04:31 +0530

    Reorganized navbar and files

--- includes/navbar.php
+++ includes/navbar.php
@@ -5,11 +5,11 @@
 
         <ul class="links flex gap-8  w-auto">
             <!-- <li><a href="" class=" font-medium not-italic text-lg duration-500 text-white no-underline hover:text-sky-500 hover:scale-105 hover:tracking-widest " >Home</a></li> -->
-            <li><a href="" class=" font-medium not-italic text-lg duration-500 text-white no-underline hover:text-sky-500 hover:scale-105 hover:tracking-widest " >ABOUT</a></li>
-            <li><a href="" class=" font-medium not-italic text-lg duration-500 text-white no-underline hover:text-sky-500 hover:scale-105 hover:tracking-widest " >GALLERY</a></li>
+            <li><a href="about.php" class=" font-medium not-italic text-lg duration-500 text-white no-underline hover:text-sky-500 hover:scale-105 hover:tracking-widest " >ABOUT</a></li>
+            <li><a href="gallery.php" class=" font-medium not-italic text-lg duration-500 text-white no-underline hover:text-sky-500 hover:scale-105 hover:tracking-widest " >GALLERY</a></li>
             <!-- <li><a href="" class=" font-medium not-italic text-lg duration-500 text-white no-underline hover:text-sky-500 hover:scale-105 hover:tracking-widest " >Services</a></li> -->
-            <li><a href="" class=" font-medium not-italic text-lg duration-500 text-white no-underline hover:text-sky-500 hover:scale-105 hover:tracking-widest " >CONTACT</a></li>
-            <li><a href="" class=" font-medium not-italic text-lg duration-500 text-sky-500 no-underline border border-sky-500 px-5 py-2.5 hover:text-white hover:scale-105 hover:bg-sky-500" >PACKAGES</a></li>
+            <li><a href="contact.php" class=" font-medium not-italic text-lg duration-500 text-white no-underline hover:text-sky-500 hover:scale-105 hover:tracking-widest " >CONTACT</a></li>
+            <li><a href="packages.php" class=" font-medium not-italic text-lg duration-500 text-sky-500 no-underline border border-sky-500 px-5 py-2.5 hover:text-white hover:scale-105 hover:bg-sky-500" >PACKAGES</a></li>
         </ul>
 
         <div class="toggle_btn text-white text-2xl cursor-pointer hidden">
@@ -20,11 +20,11 @@
 
     <div class="dropdown_menu absolute right-8 top-20 h-0 w-80 bg-transparent backdrop-blur-lg rounded-3xl overflow-hidden transition-height text-white">
         <!-- <li class=" flex justify-center items-center p-3"><a href="" class=" duration-500 hover:tracking-widest hover:text-sky-500 hover:scale-105">Home</a></li> -->
-        <li class=" flex justify-center items-center p-3"><a href="" class=" duration-500 hover:tracking-widest hover:text-sky-500 hover:scale-105">ABOUT</a></li>
-        <li class=" flex justify-center items-center p-3"><a href="" class=" duration-500 hover:tracking-widest hover:text-sky-500 hover:scale-105">GALLERY</a></li>
+        <li class=" flex justify-center items-center p-3"><a href="about.php" class=" duration-500 hover:tracking-widest hover:text-sky-500 hover:scale-105">ABOUT</a></li>
+        <li class=" flex justify-center items-center p-3"><a href="gallery.php" class=" duration-500 hover:tracking-widest hover:text-sky-500 hover:scale-105">GALLERY</a></li>
         <!-- <li class=" flex justify-center items-center p-3"><a href="" class=" duration-500 hover:tracking-widest hover:text-sky-500 hover:scale-105">SERVICES</a></li> -->
-        <li class=" flex justify-center items-center p-3"><a href="" class=" duration-500 hover:tracking-widest hover:text-sky-500 hover:scale-105">CONTACT</a></li>
-        <li class=" flex justify-center items-center p-3"><a href="" class=" duration-500 hover:tracking-widest hover:scale-105  text-sky-500 no-underline border border-sky-500 px-5 py-2.5 hover:text-white">PACKAGES</a></li>
+        <li class=" flex justify-center items-center p-3"><a href="contact.php" class=" duration-500 hover:tracking-widest hover:text-sky-500 hover:scale-105">CONTACT</a></li>
+        <li class=" flex justify-center items-center p-3"><a href="packages.php" class=" duration-500 hover:tracking-widest hover:scale-105  text-sky-500 no-underline border border-sky-500 px-5 py-2.5 hover:text-white">PACKAGES</a></li>
     </div>
 
     
