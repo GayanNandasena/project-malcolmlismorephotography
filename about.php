0477d658beb40d71345dee47327e42886ccd3940
Gayan Nandasena <gsnandasena@gmail.com>
2024-08-08 01:35:32 +0530

    Feature: About Me Page
    - Created an 'About Me' page (responsiveness to be done).

---
+++ about.php
@@ -0,0 +1,108 @@
+<?php include('includes/header.php'); ?>  <!-- header section -->
+<?php include('includes/navbar.php'); ?>  <!-- navbar section -->
+
+<body class="about-page bg-black " style=" padding-left: 15%; padding-right: 15%;">
+    <div class="bio text-white bg-black h-auto mt-36 flex mb-8">
+        <div class="profile">
+            <h1 class=" text-6xl tracking-normal">B I O G R A P H Y</h1><br>
+            <div class="h-1 w-[28.5rem] bg-gradient-to-r from-sky-500 to-blue-500"></div>
+            <img src="assets/images/profile.jpg" alt="profile" class=" pr-12 pt-12">
+        </div>
+        <div class="content h-auto">
+            <h3 class=" text-xl">Hi, I'm Malcolm Lismore.</h3><br>
+            <p>Welcome to my photography world! I'm Malcolm Lismore, a dedicated freelance photographer based on the stunning North West coast of Scotland. 
+               My passion is capturing the natural beauty that surrounds us and the special moments that make life memorable.
+               <br><br>From the rugged Scottish landscapes and intricate wildlife to the joyous celebrations of weddings and events, I bring a unique blend of artistry and professionalism to every shot.
+               With years of experience behind the lens, my work reflects a deep love for nature and a commitment to capturing each moment perfectly. 
+               <br><br>My portfolio features breathtaking landscapes, intimate wildlife portraits, and timeless wedding photography, each image telling its own story. 
+               When I'm not exploring wild terrains or documenting special occasions, I'm focused on providing personalized photography services tailored to your needs. 
+               From our initial consultation to the final photograph, I strive to ensure every detail is perfect, making your experience as memorable as the photos themselves.
+               <br><br>Explore my galleries to see the world through my eyes, and feel free to reach out with any inquiries or for bookings. Let me help you capture the moments that matter most.
+               <br><br>
+               Thank you for visiting!
+            </p>
+        </div>
+    </div>
+
+    <div class="testimonial h-auto mt-36 mb-20">
+        <div class=" flex flex-col items-center text-center">
+            <h1 class=" text-6xl tracking-normal text-white">T E S T I M O N I A L</h1><br>
+            <div class=" text-center h-1 w-[35rem] bg-gradient-to-r from-sky-500 to-blue-500"></div><br><br>
+            <p class=" text-white">I am honored to have worked with amazing clients who have trusted me to capture their special moments. Here are some of their kind words, reflecting their experiences and satisfaction with my work.</p>
+        </div>
+
+        <div class="container grid grid-cols-3 grid-rows-2 gap-4 pt-20">
+
+            <div class=" text-white text-center border border-sky-500 p-5 shadow-sm shadow-white">
+                <h1 class=" font-bold">"Malcolm captured the essence of our wedding day perfectly.</h1><br>
+                <p class=" text-gray-400">His attention to detail and ability to make everyone feel at ease resulted in stunning photos that we will cherish forever."</p><br>
+                <div class=" flex items-center justify-center">
+                    <img src="assets/images/testimonials/sarah_thompson.jpg" alt="pic" class=" w-20 h-20 bg-cover rounded-full">
+                    <div class=" text-left pl-4">
+                        <h1 class=" font-bold">Sarah Thompson</h1>
+                        <h2 class=" text-sky-500">Bride</h2>
+                    </div>
+                </div>
+            </div>
+            <div class=" text-white text-center border border-sky-500 p-5 shadow-sm shadow-white">
+                <h1 class=" font-bold">"Working with Malcolm was an absolute pleasure.</h1><br>
+                <p class=" text-gray-400">His professionalism and creativity shine through in every shot. Our corporate event photos exceeded our expectations."</p><br>
+                <div class=" flex items-center justify-center">
+                    <img src="assets/images/testimonials/james_roberston.jpg" alt="pic" class=" w-20 h-20 bg-cover rounded-full">
+                    <div class=" text-left pl-4">
+                        <h1 class=" font-bold">James Roberston</h1>
+                        <h2 class=" text-sky-500">Event Planner</h2>
+                    </div>
+                </div>
+            </div>
+            <div class=" text-white text-center border border-sky-500 p-5 shadow-sm shadow-white">
+                <h1 class=" font-bold">"Malcolm's photography is simply outstanding.</h1><br>
+                <p class=" text-gray-400">His ability to capture the beauty of nature in every shot is remarkable. I highly recommend him for any outdoor or wildlife photography."</p><br>
+                <div class=" flex items-center justify-center">
+                    <img src="assets/images/testimonials/emily_davis.jpg" alt="pic" class=" w-20 h-20 bg-cover rounded-full">
+                    <div class=" text-left pl-4">
+                        <h1 class=" font-bold">Emily Davis</h1>
+                        <h2 class=" text-sky-500">Nature Enthusiast</h2>
+                    </div>
+                </div>
+            </div>
+            <div class=" text-white text-center border border-sky-500 p-5 shadow-sm shadow-white">
+                <h1 class=" font-bold">"Malcolm made me feel completely at ease during the shoot, and the results were stunning.</h1><br>
+                <p class=" text-gray-400">His ability to capture natural beauty is unparalleled. I am thrilled with the portraits and can't wait to work with him again."</p><br>
+                <div class=" flex items-center justify-center">
+                    <img src="assets/images/testimonials/clarie_watson.jpg" alt="pic" class=" w-20 h-20 bg-cover rounded-full">
+                    <div class=" text-left pl-4">
+                        <h1 class=" font-bold">Clarie Watson</h1>
+                        <h2 class=" text-sky-500">Model</h2>
+                    </div>
+                </div>
+            </div>
+            <div class=" text-white text-center border border-sky-500 p-5 shadow-sm shadow-white">
+                <h1 class=" font-bold">"I've never felt more comfortable in front of a camera.</h1><br>
+                <p class=" text-gray-400">Malcolm's friendly and relaxed approach made our family photo session so much fun, and the results were fantastic."</p><br>
+                <div class=" flex items-center justify-center">
+                    <img src="assets/images/testimonials/mark_wilson.jpg" alt="pic" class=" w-20 h-20 bg-cover rounded-full">
+                    <div class=" text-left pl-4">
+                        <h1 class=" font-bold">Mark Wilson</h1>
+                        <h2 class=" text-sky-500">Father of two</h2>
+                    </div>
+                </div>
+            </div>
+            <div class=" text-white text-center border border-sky-500 p-5 shadow-sm shadow-white">
+                <h1 class=" font-bold">"Malcolm's talent for capturing the perfect moment is truly impressive.</h1><br>
+                <p class=" text-gray-400">His photos of our company's annual gala were a big hit, and we look forward to working with him again."</p><br>
+                <div class=" flex items-center justify-center">
+                    <img src="assets/images/testimonials/laura_bennet.jpg" alt="pic" class=" w-20 h-20 bg-cover rounded-full">
+                    <div class=" text-left pl-4">
+                        <h1 class=" font-bold">Laura Bennett</h1>
+                        <h2 class=" text-sky-500">Corporate Communications Manager</h2>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+
+<?php include('includes/footer.php'); ?> <!-- footer section -->
+
