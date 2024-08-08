fec02c63b0589a36067201d907e5592e29d2afab
Gayan Nandasena <gsnandasena@gmail.com>
2024-08-09 01:15:46 +0530

    Feature: Packages Page
    - Added a new 'Packages' page with full responsiveness.

---
+++ packages.php
@@ -0,0 +1,68 @@
+<?php include('includes/header.php'); ?>  <!-- header section -->
+<?php include('includes/navbar.php'); ?>  <!-- navbar section -->
+
+<body class="packages-page bg-black max-lg:p-0">
+    
+    <div class="packages mt-20 lg:mt-36 text-white text-center tracking-widest" style="margin-left: 5%; margin-right: 5%;">
+        <h1 class=" text-2xl lg:text-5xl font-bold tracking-widest">P A C K A G E S</h1><br>
+        <div class="h-1 w-auto bg-gradient-to-r from-sky-500 to-blue-500"></div>
+        <p class=" pt-12">Discover the perfect photography package to capture your special moments with Malcolm Lismore Photography. 
+            Whether it's a small event, a full-day celebration, or a personalized portrait session, these tailored packages offer a range of options to suit your needs and create lasting memories.</p>
+
+        <div class=" grid grid-rows-4 grid-cols-1 lg:grid-rows-1 lg:grid-cols-4 gap-6">
+            <div class="essential-package mt-20 p-6 text-center border border-sky-500 shadow-sm shadow-white duration-500 hover:scale-110 cursor-pointer">
+                <h2 class="tracking-widest text-gray-200">E S S E N T I A L</h2><br>
+                <h1 class=" text-4xl text-sky-400">Price: £250</h1><br>
+                <p class=" tracking-wide text-gray-400 text-left pl-5"> ⦾ Duration/2hours
+                    <br>⦾ Small event or Photoshoot.
+                    <br>⦾ 20 high-resolution edited images.
+                    <br>⦾ Online gallery for viewing and downloading images.
+                </p><br><br><br>
+                <br><button class="px-8 py-2 border border-white hover:bg-sky-500">Select Now</button>
+            </div>
+            <div class="standard-package mt-20 p-6 text-center border bg-gray-200 border-sky-500 shadow-sm shadow-white duration-500 hover:scale-110 cursor-pointer">
+                <h2 class="tracking-widest text-black">S T A N D A R D</h2><br>
+                <h1 class=" text-4xl text-sky-400">Price: £450</h1><br>
+                <p class=" tracking-wide text-black text-left pl-5"> ⦾ Duration/4hours
+                    <br>⦾ half-day events or portrait sessions.
+                    <br>⦾ 40 high-resolution edited images.
+                    <br>⦾ Online gallery with password protection.
+                    <br>⦾ 1 print (8x10) of your choice.
+                </p><br><br>
+                <button class="px-8 py-2 border border-sky-500 text-sky-500 hover:bg-sky-500 hover:text-white">Select Now</button>
+            </div>
+            <div class="premium-package mt-20 p-6 text-center border border-sky-500 shadow-sm shadow-white duration-500 hover:scale-110 cursor-pointer">
+                <h2 class="tracking-widest text-gray-200">P R E M I U M</h2><br>
+                <h1 class=" text-4xl text-sky-400">Price: £900</h1><br>
+                <p class=" tracking-wide text-gray-400 text-left pl-5"> ⦾ Duration/8hours
+                    <br>⦾ weddings, events, or extensive portrait sessions.
+                    <br>⦾ 80 high-resolution edited images.
+                    <br>⦾ Online gallery with password protection.
+                    <br>⦾ 2 prints(8x10) plus USB
+                </p><br><br>
+                <button class="px-8 py-2 border border-white hover:bg-sky-500">Select Now</button>
+            </div>
+            <div class="ultimate-package mt-20 p-6 text-center border bg-gray-200 border-sky-500 shadow-sm shadow-white duration-500 hover:scale-110 cursor-pointer">
+                <h2 class="tracking-widest text-black">U L T I M A T E</h2><br>
+                <h1 class=" text-4xl text-sky-400">Price: £1500</h1><br>
+                <p class=" tracking-wide text-black text-left pl-5"> ⦾ Full Day/additional as needed.
+                    <br>⦾ Start to finish event coverage.
+                    <br>⦾ 120 high-resolution edited images.
+                    <br>⦾ Online gallery with password protection.
+                    <br>⦾ 4 print(8x10), Custom Album with USB.
+                </p><br><br>
+                <button class="px-8 py-2 border border-sky-500 text-sky-500 hover:bg-sky-500 hover:text-white">Select Now</button>
+            </div>
+        </div>
+
+        <p class=" p-4 lg:p-20 text-sm lg:text-lg">Each package is thoughtfully designed to provide you with exceptional service and stunning images that you can cherish forever. 
+            Choose the package that best fits your vision, and let me help you preserve the moments that matter most.</p>
+    </div>
+
+    <!-- Bookings Section -->
+    <div class="bookings bg-white mt-20 px-0 h-96 text-black text-center">
+        <h1 class=" text-2xl lg:text-5xl font-bold tracking-widest pt-12">B O O K I N G S</h1>
+    </div>
+
+
+<?php include('includes/footer.php'); ?> <!-- footer section -->
