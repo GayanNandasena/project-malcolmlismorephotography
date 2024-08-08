4225a02279a3a8c8202c70bcb6198fe76ca8fd05
Gayan Nandasena <gsnandasena@gmail.com>
2024-08-09 01:33:55 +0530

    Added Developer Note

--- includes/header.php
+++ includes/header.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Malcolm Lismore Photography</title>
+    <link rel="icon" type="image/x-icon" href="">
     
     <script src="https://cdn.tailwindcss.com"></script>
     <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.4.7/flowbite.min.css" rel="stylesheet">
@@ -11,6 +12,13 @@
     <link rel="stylesheet" href="styles.css">
     
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+    <!--
+        Developer: Gayan Nandasena
+        Note: This web solution was developed by Gayan Nandasena as part of a college project.
+        Unauthorized removal of this comment is not permitted.
+        August 2024
+    -->
 
 </head>
 
